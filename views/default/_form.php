001190d93eeee13de0f847d2186088695fc0a3e7
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2019-07-12 14:40:34 +0300

    refactor theme

--- views/default/_form.php
+++ views/default/_form.php
@@ -83,7 +83,7 @@ use artsoft\widgets\LanguagePills;
                             <?php else: ?>
                                 <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
                                 <?= Html::a(Yii::t('art', 'Delete'), ['/seo/default/delete', 'id' => $model->id], [
-                                    'class' => 'btn btn-default',
+                                    'class' => 'btn btn-danger',
                                     'data' => [
                                         'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
                                         'method' => 'post',
