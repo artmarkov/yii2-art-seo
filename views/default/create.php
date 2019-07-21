c71c12998402abdd3da4219e47113308944149ee
artmarkov <artmarkov@mail.ru>
2019-07-21 21:43:17 +0300

    refactor theme

--- views/default/create.php
+++ views/default/create.php
@@ -10,6 +10,10 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('art/seo', 'SEO'), 'url' => 
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="seo-create">
-    <h3 class="lte-hide-title"><?= Html::encode($this->title) ?></h3>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3 class="page-title"><?=  Html::encode($this->title) ?></h3>            
+        </div>
+    </div>
     <?= $this->render('_form', compact('model')) ?>
 </div>
